1b6c936b61ae444b6a17f632efdca3665dc62589
bella <yuw170@ucsd.edu>
2024-06-08 17:14:18 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Covid_19_prison/', // Set the base path for GitHub Pages deployment
+  base: '/Covid19-vis/', // Set the base path for GitHub Pages deployment
   plugins: [svelte()],
 });
